b809033b5101cc49e7b87d8a79e31746a8f78108
dawkin <61014353+dawkin@users.noreply.github.com>
2022-09-28 12:24:46 +0200

    bug fix: data type for FLANN matching

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -18,7 +18,7 @@ void matchDescriptors(std::vector<cv::KeyPoint> &kPtsSource, std::vector<cv::Key
     }
     else if (matcherType.compare("MAT_FLANN") == 0)
     {
-        if (descSource.type() != CV_32F)
+        if (descSource.type() != CV_32F || descRef.type() != CV_32F)
         { // OpenCV bug workaround : convert binary descriptors to floating point due to a bug in current OpenCV implementation
             descSource.convertTo(descSource, CV_32F);
             descRef.convertTo(descRef, CV_32F);
